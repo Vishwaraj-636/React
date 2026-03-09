2c3a222058d1e7d6d0cbfb9cb69755b6196bafe5
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-03-09 10:13:38 +0530

    class-0

---
+++ class0/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello guys, i am vishwaraj</h1>
+      <h2>hello world</h2>
+    </div>
+  )
+}
+
+export default App

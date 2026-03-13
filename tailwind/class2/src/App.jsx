1735075d4384a93385df2e6f6cb4817967486a98
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-03-13 10:56:32 +0530

    'class2-tailwind'

---
+++ tailwind/class2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold'>hello world</h1>
+      <h1 className='bg-emerald-600 text-9xl'>vishwaraj</h1>
+    </div>
+  )
+}
+
+export default App

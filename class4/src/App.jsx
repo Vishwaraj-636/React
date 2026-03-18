50551d9e841a249ba9afed47c1fe14719e9d0d64
Vishwaraj Singh Shekhawat <vishwaraj.singh1523@gmail.com>
2026-03-18 21:53:09 +0530

    class4-useState

--- class4/src/App.jsx
+++ class4/src/App.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  const [marks, setmarks] = useState([60,55,89,12,29])
 
-  const [num, setnum] = useState(0)
-  function increase() {
-    setnum(num+1)
+  function giveThemGrace() {
+    const newMarks =marks.map(function (ele) {
+      return ele+5
+    })
+    console.log(newMarks)
+    setmarks(newMarks)
   }
-  function decrease() {
-    setnum(num-1)
-  }
+
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={increase}>increase</button>
-      <button onClick={decrease}>decrease</button>
+      {marks.map(function (elem, idx) {
+        return <h1 key={idx}>Student {idx + 1} = {elem} ({elem>33?"PASS":"FAIL"})</h1>
+      })}
+      <button onClick={giveThemGrace}>Give grace marks</button>
     </div>
   )
 }
